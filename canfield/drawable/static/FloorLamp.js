85d6463eb2ecc4e4d505c93f0520618e0e24ba08
Christopher D Canfield <cdcanfield@gmail.com>
2013-12-17 04:29:13 -0500

    More FloorLamp updates

--- canfield/drawable/static/FloorLamp.js
+++ canfield/drawable/static/FloorLamp.js
@@ -17,29 +17,29 @@ function FloorLamp(xLeft, yBottom, zBack, zone) {
     Drawable.call(this);
     zone.addDrawable(this);
     
-    var poleTexture = cdc.textureManager.getTexture(texturePath);
+    var poleTexture = cdc.textureManager.getTexture(Textures.METAL_2);
     var material = new THREE.MeshLambertMaterial({ 
         map: texture
     });
-    poleTexture.repeat.set(textureRepeatX, textureRepeatY);
+    poleTexture.repeat.set(1, 1);
     
     this.poleGeometry = new THREE.CylinderGeometry(2, 6, 20, 8, 1, true);
     var poleMesh = new THREE.Mesh(this.poleGeometry, material);
     poleMesh.position.set(xLeft, yBottom, zBack);
     this.threeJsDrawable = poleMesh;
     
-    var bulbTexture = cdc.textureManager.getTexture(texturePath);
+    var bulbTexture = cdc.textureManager.getTexture(Textures.FIRE_4);
     var material = new THREE.MeshBasicMaterial({ 
         map: texture
     });
-    bulbTexture.repeat.set(textureRepeatX, textureRepeatY);
+    bulbTexture.repeat.set(1, 1);
     
-    var bulbGeometry = new THREE.CubeGeometry(width, height, depth);
+    var bulbGeometry = new THREE.CubeGeometry(6, 8, 6);
     var bulbMesh = new THREE.Mesh(bulbGeometry, material);
     bulbMesh.position.set(xLeft, yBottom, zBack);
     poleMesh.add(bulbMesh);
     
-    
+    // TODO: Add light & particle system (continuous, up)
     
     
     // Prevent camera from walking through light.
